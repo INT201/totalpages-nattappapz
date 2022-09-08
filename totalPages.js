acc6f8017683ebc0f762040ea647802a584c2ed7
Nattaphat <nattaphat.ngar@kmutt.ac.th>
2022-09-08 22:29:27 +0700

    totalpagesPush

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,11 @@
 const { template } = require('@babel/core')
-
+/*
+กรณี arrayItems เป็น null หรือ undefined ให้ return undefined
+กรณี rowsPerPage เป็น null หรือ undefined หรือเป็น 0 จะ return 1
+*/
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  arrayItems == null || undefined? undefined:arrayItems
+  rowsPerPage == null || undefined || 0? 1:rowsPerPage
+  return Math.ceil(arrayItems.length/rowsPerPage)
 }
 module.exports = totalPages
